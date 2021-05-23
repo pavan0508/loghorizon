bb496e42465e1fb6b556845b0ac4b326b150ec3c
pavan0508 <69718388+pavan0508@users.noreply.github.com>
2021-05-23 06:26:46 +0530

    Update prgkiller.py

--- prgkiller.py
+++ prgkiller.py
@@ -3,11 +3,12 @@ import time
 print(os.getcwd())
 print(os.system("ls -al"))
 t = input("give the time to delete the program")
+f = input("name of the file U want to Delete?")
 n = 1
 while(n<t):
 	if(n==t-1):
 		os.system("clear")
-		os.system("rm -f a2.py")
+		os.system("rm -f {}".format(f))
 		print("deletd")
 	n += 1
 	time.sleep(1)
